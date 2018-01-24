85c85b0fa529cb6b547903ee368e24b7de17125f
David Hahn <d.hahn@salt-and-pepper.eu>
2018-01-24 16:41:12 +0100

    Change return value of getMenuItem to Promise<MenuItem>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as path from 'path'
 
 import { Application, AppConstructorOptions } from 'spectron'
+import { MenuItem } from 'electron'
 
 export class SpectronMenuAddon {
   private app: Application
@@ -20,7 +21,7 @@ export class SpectronMenuAddon {
     this.app.electron.ipcRenderer.send('SPECTRON_MENU_ADDON/CLICK_MENU_ITEM', labels)
   }
 
-  async getMenuItem(...labels: string[]) {
+  async getMenuItem(...labels: string[]): Promise<MenuItem> {
     return await this.app.electron.ipcRenderer.sendSync('SPECTRON_MENU_ADDON/GET_MENU_ITEM', labels)
   }
 }
